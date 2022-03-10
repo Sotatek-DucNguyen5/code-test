df01481bdecc58419954a612b8b27e94abd26718
theanh.nguyen1 <theanh.nguyen@mcpayment.net>
2022-03-10 10:17:44 +0700

    feat: result response

--- src/main/java/com/sotatek/codingtest/controller/PostDetailController.java
+++ src/main/java/com/sotatek/codingtest/controller/PostDetailController.java
@@ -1,10 +1,10 @@
 package com.sotatek.codingtest.controller;
 
 import com.sotatek.codingtest.domain.dto.PostDetailDTO;
+import com.sotatek.codingtest.helper.ResultResp;
 import com.sotatek.codingtest.service.postdetail.PostDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +16,28 @@ public class PostDetailController {
     private PostDetailService postDetailService;
 
     @GetMapping("get-all")
-    public ResponseEntity<List<PostDetailDTO>> getAll() {
-        return new ResponseEntity<>(postDetailService.findAll(), HttpStatus.OK);
+    public ResultResp<List<PostDetailDTO>> getAll() {
+        List<PostDetailDTO> postDetailDTOList = postDetailService.findAll();
+        if (postDetailDTOList.size() <= 0) {
+            return new ResultResp<>(HttpStatus.OK);
+        }
+        return new ResultResp<>(HttpStatus.OK, postDetailDTOList);
     }
 
     @GetMapping("get-by-postcodes")
-    public ResponseEntity<List<PostDetailDTO>> getAllByPostcodes(@RequestBody List<Long> postcodes) {
-        return new ResponseEntity<>(postDetailService.findAllByPostcode(postcodes), HttpStatus.OK);
+    public ResultResp<List<PostDetailDTO>> getAllByPostcodes(@RequestBody List<Long> postcodes) {
+        List<PostDetailDTO> postDetailDTOList = postDetailService.findAllByPostcode(postcodes);
+        if (postDetailDTOList.size() <= 0) {
+            return new ResultResp<>(HttpStatus.OK);
+        }
+        return new ResultResp<>(HttpStatus.OK, postDetailDTOList);
     }
 
     @PostMapping("create")
-    public ResponseEntity<PostDetailDTO> create(@RequestBody PostDetailDTO postDetailDTO) {
-        return new ResponseEntity<>(postDetailService.save(postDetailDTO), HttpStatus.CREATED);
+    public ResultResp<PostDetailDTO> create(@RequestBody PostDetailDTO postDetailDTO) {
+        if (postDetailDTO.getSuburbNames() == null || postDetailDTO.getPostcodes() == null) {
+            return new ResultResp<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResultResp<>(HttpStatus.CREATED, postDetailService.save(postDetailDTO));
     }
 }
